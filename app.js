e26152a799b582dfaf6cde677dd64c2c5faa6b4b
yosr thabet <yosr.thabet@isgb.ucar.tn>
2024-11-29 13:36:10 +0100

    creation du fichier app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port =3000;
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port,() => {
+    console.log(`application exemple a l'ecoute sur le port  ${port}`)
+});
+
