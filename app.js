1b7ec79b3afbfe3ca36b8ca42d81eab93c46fc8b
Ghofrane Rezgui Hmema <ghofranerezgui9@gmail.com>
2024-12-06 20:52:09 +0100

    nouveau app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const express = require('express');
-<<<<<<< HEAD
+const express = require('express'); 
 const dotenv = require('dotenv');
 const connectDB = require('./config/db'); 
 
@@ -13,9 +12,7 @@ dotenv.config();
 const app = express();
 const port = 5000;
 
-
 connectDB();
-
 
 app.use(logger);
 
@@ -26,7 +23,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/users', UserRoutes);
 app.use('/api/ideas', IdeaRoutes);
 
-
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Erreur interne du serveur' });
@@ -35,37 +31,3 @@ app.use((err, req, res, next) => {
 app.listen(port, () => {
     console.log(`Application à l'écoute sur le port ${port}`);
 });
-=======
-const app = express();
-const port =3000;
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-
-app.use(express.json()); 
-app.use(cors());
-
-
-app.use("/api/auth", require("./routes/authRoutes")); // Routes pour l'authentification
-app.use("/api/ideas", require("./routes/ideaRoutes")); // Routes pour les idées
-app.use("/api/users", require("./routes/userRoutes")); // Routes pour les utilisateurs
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(port,() => {
-    console.log(`application exemple a l'ecoute sur le port  ${port}`)
-});
-
->>>>>>> fbe802f2f369c0927e3ebb43226dc0d4415daa1d
